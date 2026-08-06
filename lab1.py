c6a14c22ef001cc64f8c6c807eda083f56476f83
ashitoshdaingade1101-spec <ashitoshdaingade1101@gmail.com>
2026-08-06 15:40:35 +0530

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,10 @@
+n1 =2
+n2 =3
+num = n1 < n2
+print("value of num =", num)
+num = n1 > n2
+print("value of num ", num)
+num = n1 < n2
+print("value of num ", num)
+num = n1 <= n2
+print("value of num ", num)
